a5019bcfa48f48d9094157e1f5bc4dc0ee59c6c8
nov segal <novit100@gmail.com>
2020-10-23 09:56:21 +0300

    we added a private static function Welcome 0847 and invited my parrtner to write the func Welcome7224

---
+++ dotNet5781_7224_0847/dotNet5781_00_7224_0847/Program0847.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0847_7224
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7224();
+            Welcome0847();
+
+            Console.ReadKey();
+        }
+
+        private static void Welcome0847()
+        {
+            Console.WriteLine("Enter your name: ");
+            String n = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", n);
+        }
+        static partial void Welcome7224();
+    }
+}
