1e8d65ed85720a347483bb7582210898dfb87619
estizeiger <estizeiger@gmail.com>
2020-10-23 16:30:55 +0300

    i added a new class and built the body of the function my parner declared of

---
+++ dotNet5781_7224_0847/dotNet5781_00_7224_0847/Program7224.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0847_7224
+{
+    partial class Program
+    {
+        static partial void Welcome7224()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
