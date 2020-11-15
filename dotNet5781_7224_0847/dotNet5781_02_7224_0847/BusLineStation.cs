4f8a7ec4c7bca56721be57058161db4cc9529fd3
estizeiger <estizeiger@gmail.com>
2020-11-15 23:03:39 +0200

    כ

--- dotNet5781_7224_0847/dotNet5781_02_7224_0847/BusLineStation.cs
+++ dotNet5781_7224_0847/dotNet5781_02_7224_0847/BusLineStation.cs
@@ -9,29 +9,23 @@ namespace dotNet5781_02_7224_0847
     class BusLineStation
     {
         private static Random r = new Random();
-        public BusStation busStation
-        {
-            get { return busStation; }////////////////
-            private set { busStation = value; }//////
-        }
-        private double distance1;
+        private BusStation busStation = new BusStation();
+
+       public double distance1;
         public double distance
         {
             get { return distance1; }
             set
             {
-                double lat = r.NextDouble() * (33.3 - 31) + 31;
-                double lon = r.NextDouble() * (33.3 - 31) + 31;
-                distance1 = Math.Sqrt(Math.Pow(lat - busStation.latitude, 2) - Math.Pow(lon - busStation.longitude, 2));
+                //double lat = r.NextDouble() * (33.3 - 31) + 31;
+                //double lon = r.NextDouble() * (33.3 - 31) + 31;
+                //distance1 = Math.Sqrt(Math.Pow(lat - busStation.latitude, 2) - Math.Pow(lon - busStation.longitude, 2));
+                
+                distance1 = r.NextDouble()*(2000-500)+500;//distance btween stations between 500-2000 m
             }
         }
 
-        private TimeSpan time1;
-        public TimeSpan time
-        {
-            get { return time1; }
-
-        }
+        private TimeSpan time1=new TimeSpan(0, 0, seconds:(int)(distance1 * 0.05));//we assume that it takes 0.05 seconds per meter.
 
         public override string ToString()
         {
