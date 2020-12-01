3000c93bb8616bbce386b5b9947a1be8c3998646
estizeiger <estizeiger@gmail.com>
2020-12-01 23:17:05 +0200

    9

--- dotNet5781_7224_0847/dotNet5781_03A_7224_0847/MainWindow.xaml.cs
+++ dotNet5781_7224_0847/dotNet5781_03A_7224_0847/MainWindow.xaml.cs
@@ -20,8 +20,9 @@ namespace dotNet5781_03A_7224_0847
     /// </summary>
     public partial class BusesDisplay : Window
     {
-        private BusLineCollections busLineColl;
-        public BusesDisplay()
+        private BusLineCollections busLineColl;//the collection of buses
+
+        public BusesDisplay()//ctor
         {
 
             InitializeComponent();
@@ -34,12 +35,12 @@ namespace dotNet5781_03A_7224_0847
         }
         private BusLine currentDisplayBusLine;
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)//selction of a specific bus
         {
             ShowBusLine((cbBusLines.SelectedValue as BusLine).busLine);
         }
 
-        private void ShowBusLine(int busLineNum)
+        private void ShowBusLine(int busLineNum)//show all stations of the chosen bus line
         {
             currentDisplayBusLine = busLineColl[busLineNum];//thanks to the indexer
 
@@ -56,14 +57,15 @@ namespace dotNet5781_03A_7224_0847
 
             for(int i=0; i<10; i++)//add 10 buses
             {
-                int busLineNum = r.Next(1, 999);
+                int busLineNum = r.Next(1, 999);//randomal bus line number
 
                 List<BusLineStation> stat = new List<BusLineStation>();
                 int numOfStations = r.Next(2, 30);
 
-                int firstStationNum = r.Next(5000, 999999);//1st station
+                int firstStationNum = r.Next(5000, 999999);//init 1st station
                 BusLineStation first = new BusLineStation(firstStationNum, true);
                 stat.Add(first);
+
                 for (int j = 0; j < numOfStations-1; j++)//the other stations
                 {
                     int stationNum = r.Next(5000, 999999);
