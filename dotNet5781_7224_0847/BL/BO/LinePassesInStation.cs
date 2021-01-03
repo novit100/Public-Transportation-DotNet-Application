817f42ac46e8225d7c07feee504a23f076cc19fe
estizeiger <estizeiger@gmail.com>
2021-01-03 20:05:54 +0200

    6

---
+++ dotNet5781_7224_0847/BL/BO/LinePassesInStation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class LinePassesInStation
+    {
+        //public int LineId { get; set; }
+        //public int BusNumber { get; set; }
+        //public int LastStation { get; set; }
+    }
+}
---
+++ dotNet5781_7224_0847/BL/BO/Station.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Station
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Longitude { get; set; }
+        public double Lattitude { get; set; }
+        IEnumerable<Line> lines { set; get; }
+        //IEnumerable<LinePassesInStation> linesInStation { set; get; }
+    }
+}
