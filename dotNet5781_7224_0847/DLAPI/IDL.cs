b86ed3f048003fc85d2d9f7051ab17e51c7b2593
estizeiger <estizeiger@gmail.com>
2021-01-03 16:05:39 +0200

    add all project files again

---
+++ dotNet5781_7224_0847/DLAPI/IDL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+//using DO;
+
+namespace DLAPI
+{
+    //CRUD Logic:
+    // Create - add new instance
+    // Request - ask for an instance or for a collection
+    // Update - update properties of an instance
+    // Delete - delete an instance
+    public interface IDL
+    {
+
+    }
+}
---
+++ dotNet5781_7224_0847/PL/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using BLAPI;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IBL bl = BLFactory.GetBL("1");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGO_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbStations.IsChecked == true)
+            {
+                StationsWindow win = new StationsWindow(bl);
+                win.Show();
+            }
+            else
+            {
+                LinesWindow win = new LinesWindow(bl);
+                win.Show();
+            }
+
+        }
+    }
+}
